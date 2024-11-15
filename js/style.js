f8bcdd7bf1d6a15214d6bbb15035cb473eee5b5d
BoladeOlalekan <lekanseyibolade@gmail.com>
2024-11-15 15:39:05 +0100

    (Updated birthday function)

--- js/style.js
+++ js/style.js
@@ -1,87 +1,190 @@
-//Declaring variables
-var day_input = document.getElementById('day');
-var month_input = document.getElementById('month');
-var year_input = document.getElementById('year');
-//var date = new date();
-var dayMsg = document.querySelector('.day-msg');
-var monthMsg = document.querySelector('.month-msg');
-var yearMsg = document.querySelector('.year-msg');
-var submit = document.querySelector('.img-btn');
-var head1 = document.querySelector(".day-header");
-var head2 = document.querySelector(".month-header");
-var head3 = document.querySelector(".year-header");
+//DECLARING constIABLES
+//INPUTS
+const day_input = document.getElementById('day');
+const month_input = document.getElementById('month');
+const year_input = document.getElementById('year');
 
-// Validate input values
-var isValid = true;~
+//OUTPUTS
+const submit = document.querySelector('.img-btn');
+const dayOtp = document.getElementById('calc-days');
+const monthOtp = document.getElementById('calc-month');
+const yearOtp = document.getElementById('calc-year');
 
-submit.addEventListener('click' , () => {
+//ERRORS
+const dayMsg = document.querySelector('.day-msg');
+const monthMsg = document.querySelector('.month-msg');
+const yearMsg = document.querySelector('.year-msg');
+const dayHead = document.querySelector(".day-header");
+const monthHead = document.querySelector(".month-header");
+const yearHead = document.querySelector(".year-header");
+const requiredError = 'This field is required';
+const dayError = 'Must be a valid date';
+const monthError = 'Must be a valid month';
+const yearError = 'Must be in the past';
+
+//DATES
+var today = new Date();
+var currentYear = today.getFullYear();
+var currentMonth = 1 + today.getMonth();
+var currentDay = today.getDate();
+const month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+//const months = [31,28,31,30,31,30,31,31,30,31,30,31];
+
+
+function checkDayInput(){   
+    let value = day_input.value;
+    if(value == ''){
+        day_input.style.borderColor = 'hsl(0, 100%, 67%)';
+        dayHead.style.color = 'hsl(0, 100%, 67%)';
+        dayMsg.innerHTML = requiredError;
+        return false;
+    }else if(value < 1 || value > 31)
+    {
+        day_input.style.borderColor = 'hsl(0, 100%, 67%)';
+        dayHead.style.color = 'hsl(0, 100%, 67%)';
+        dayMsg.innerHTML = dayError;
+        return false;
+    }else
+    {
+        day_input.style.borderColor = '';
+        dayHead.style.color = '';
+        dayMsg.innerHTML = '';
+        return true;
+    }
+}
+
+function checkMonthInput(){
+    let value = month_input.value;
+    if(value == ''){
+        month_input.style.borderColor = 'hsl(0, 100%, 67%)';
+        monthHead.style.color = 'hsl(0, 100%, 67%)';
+        monthMsg.innerHTML = requiredError;
+        return false;
+    }else if(value < 1 || value > 12)
+    {
+        month_input.style.borderColor = 'hsl(0, 100%, 67%)';
+        monthHead.style.color = 'hsl(0, 100%, 67%)';
+        monthMsg.innerHTML = monthError;
+        return false;
+    }else
+    {
+        month_input.style.borderColor = '';
+        monthHead.style.color = '';
+        monthMsg.innerHTML = '';
+        return true;
+    }
+}
+
+function checkYearInput(){
+    let value = year_input.value;
+
+    if(value == ''){
+        year_input.style.borderColor = 'hsl(0, 100%, 67%)';
+        yearHead.style.color = 'hsl(0, 100%, 67%)';
+        yearMsg.innerHTML = requiredError;
+        return false;
+    }else if(value < 1 || value > currentYear)
+    {
+        year_input.style.borderColor = 'hsl(0, 100%, 67%)';
+        yearHead.style.color = 'hsl(0, 100%, 67%)';
+        yearMsg.innerHTML = yearError;
+        return false;
+    }else
+    {
+        year_input.style.borderColor = '';
+        yearHead.style.color = '';
+        yearMsg.innerHTML = '';
+        return true;
+    }
+}
+
+function calculateAge(birthday){
+    var fullBirthDate = new Date(birthday);
+    var birthDay = fullBirthDate.getDate();
+    var birthMonth = fullBirthDate.getMonth();
+    var birthYear = fullBirthDate.getFullYear();
+
+    var years = currentYear - birthYear;
+    var months = currentMonth - birthMonth;
+    var days = currentDay - birthDay;
+
+    if(months < 0 || (months === 0 && days < 0)){
+        years--;
+        if (months === 0){
+            months = 11;
+        } else{
+            months = 12 + months;
+        }
+        days = 30 + days;
+    }
+
+    yearOtp.innerHTML = years;
+    monthOtp.innerHTML = months;
+    dayOtp.innerHTML = days;
+
+    /*let today = new Date();
+    let currentYear = today.getFullYear();
+    let currentMonth = 1 + today.getMonth();
+    let currentDay = today.getDate();
+    const month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    if(day_input > currentDay){
+        currentDay = currentDay + month_days[currentMonth - 1];
+        currentMonth = currentMonth - 1;
+    }
+    if(month_input > currentMonth){
+        currentMonth = currentMonth + 12;
+        currentYear = currentYear - 1;
+    }*/
+
+
+   
+}
+
+submit.addEventListener('click' , function (e) {
+    e.preventDefault();
+    let day = checkDayInput();
+    let month = checkMonthInput();
+    let year = checkYearInput();
+
+    if(!day || !month || !year){
+        return false;
+    }
+    let birthday = `${month_input.value}/${day_input.value}/${year_input.value}`;
+    console.log(birthday);
+    calculateAge(birthday);
+    console.log(calculateAge());
+
+    typeof (day);
+    typeof (month);
+    typeof (year);
+
+    console.log(typeof(day));
+    console.log(typeof(month));
+    console.log(typeof(year));
+})
+
+
+/*submit.addEventListener('click' , () => {
     const months = [31,28,31,30,31,30,31,31,30,31,30,31];
     let today = new Date();
     let currentYear = today.getFullYear();
-    let currentMonth = today.getMonth()+1;
+    let currentMonth = today.getMonth() + 1;
     let currentDay = today.getDate();
+    const isValid = true;
 
     //Validity for empty and non-empty input
-    if(typeof day_input.value, month_input.value, year_input.value == ""){
-        console.log(typeof day_input.value, month_input.value, year_input.value);
-        isValid = false;
-        dayMsg.innerHTML = 'This field is required';
-        monthMsg.innerHTML = 'This field is required';
-        yearMsg.innerHTML = 'This field is required';
-        head1.style.color = 'hsl(0, 100%, 67%)';
-        head2.style.color = 'hsl(0, 100%, 67%)';
-        head3.style.color = 'hsl(0, 100%, 67%)';
-        day_input.style.borderColor = 'hsl(0, 100%, 67%)';
-        month_input.style.borderColor = 'hsl(0, 100%, 67%)';
-        year_input.style.borderColor = 'hsl(0, 100%, 67%)';
-        return;
-    }
 
-    if(typeof day_input.value > '31'){
-        console.log(typeof day_input);
-        isValid = false;
-        dayMsg.innerHTML = 'Must be a valid day';
-        head1.style.color = 'hsl(0, 100%, 67%)';
-        head2.style.color = 'hsl(0, 100%, 67%)';
-        head3.style.color = 'hsl(0, 100%, 67%)';
+    /*if(day_input.value > 31){
         day_input.style.borderColor = 'hsl(0, 100%, 67%)';
-        month_input.style.borderColor = 'hsl(0, 100%, 67%)';
-        year_input.style.borderColor = 'hsl(0, 100%, 67%)';
-        return;
-    } else{
-        isValid = true;
-        dayMsg.innerHTML = "";
-    }
-
-    if(typeof month_input.value > currentDay){
-        console.log(typeof month_input);
+        dayHead.style.color = 'hsl(0, 100%, 67%)';
+        dayMsg.innerHTML = 'Must be a valid date';
         isValid = false;
-        monthMsg.innerHTML = 'Must be a valid month';
-        head1.style.color = 'hsl(0, 100%, 67%)';
-        head2.style.color = 'hsl(0, 100%, 67%)';
-        head3.style.color = 'hsl(0, 100%, 67%)';
-        day_input.style.borderColor = 'hsl(0, 100%, 67%)';
-        month_input.style.borderColor = 'hsl(0, 100%, 67%)';
-        year_input.style.borderColor = 'hsl(0, 100%, 67%)';
         return;
-    } else{
-        isValid = true;
-        monthMsg.innerHTML = "";
     }
-
-    if(typeof year_input.value > currentYear){
-        console.log(typeof year_input);
-        isValid = false;
-        yearMsg.innerHTML = 'Must be in the past';
-        head1.style.color = 'hsl(0, 100%, 67%)';
-        head2.style.color = 'hsl(0, 100%, 67%)';
-        head3.style.color = 'hsl(0, 100%, 67%)';
-        day_input.style.borderColor = 'hsl(0, 100%, 67%)';
-        month_input.style.borderColor = 'hsl(0, 100%, 67%)';
-        year_input.style.borderColor = 'hsl(0, 100%, 67%)';
-        return;
-    } else{
+    else{
+        alert('You can proceed');
         isValid = true;
-        yearMsg.innerHTML = "";
     }
-})
+})*/
