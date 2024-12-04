44c57724f894a5945c64edccdf82b87926d0e7d7
BoladeOlalekan <lekanseyibolade@gmail.com>
2024-12-04 12:43:20 +0100

    Nan Output

--- js/style.js
+++ js/style.js
@@ -1,4 +1,4 @@
-//DECLARING constIABLES
+//DECLARING VARIABLES
 //INPUTS
 const day_input = document.getElementById('day');
 const month_input = document.getElementById('month');
@@ -26,11 +26,7 @@ const yearError = 'Must be in the past';
 var today = new Date();
 var currentYear = today.getFullYear();
 var currentMonth = 1 + today.getMonth();
-var currentDay = today.getDate();
-const month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
-//const months = [31,28,31,30,31,30,31,31,30,31,30,31];
-
+var currentDay = today.getDate()
 
 function checkDayInput(){   
     let value = day_input.value;
@@ -84,7 +80,7 @@ function checkYearInput(){
         yearHead.style.color = 'hsl(0, 100%, 67%)';
         yearMsg.innerHTML = requiredError;
         return false;
-    }else if(value < 1 || value > currentYear)
+    }else if(value < 1 || value > 2024)
     {
         year_input.style.borderColor = 'hsl(0, 100%, 67%)';
         yearHead.style.color = 'hsl(0, 100%, 67%)';
@@ -119,27 +115,13 @@ function calculateAge(birthday){
         days = 30 + days;
     }
 
-    yearOtp.innerHTML = years;
-    monthOtp.innerHTML = months;
-    dayOtp.innerHTML = days;
+    yearOtp.textContent= years;
+    monthOtp.textContent= months;
+    dayOtp.textContent= days;
 
-    /*let today = new Date();
-    let currentYear = today.getFullYear();
-    let currentMonth = 1 + today.getMonth();
-    let currentDay = today.getDate();
-    const month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
-    if(day_input > currentDay){
-        currentDay = currentDay + month_days[currentMonth - 1];
-        currentMonth = currentMonth - 1;
-    }
-    if(month_input > currentMonth){
-        currentMonth = currentMonth + 12;
-        currentYear = currentYear - 1;
-    }*/
-
-
-   
+    console.log(years);
+    console.log(months);
+    console.log(days);
 }
 
 submit.addEventListener('click' , function (e) {
@@ -148,43 +130,13 @@ submit.addEventListener('click' , function (e) {
     let month = checkMonthInput();
     let year = checkYearInput();
 
-    if(!day || !month || !year){
-        return false;
-    }
-    let birthday = `${month_input.value}/${day_input.value}/${year_input.value}`;
+    if(!day || !month || !year) return
+    let birthday = (`${day_input.value}/${month_input.value}/${year_input.value}`);
+    calculateAge();
+
     console.log(birthday);
-    calculateAge(birthday);
     console.log(calculateAge());
-
-    typeof (day);
-    typeof (month);
-    typeof (year);
-
     console.log(typeof(day));
     console.log(typeof(month));
     console.log(typeof(year));
 })
-
-
-/*submit.addEventListener('click' , () => {
-    const months = [31,28,31,30,31,30,31,31,30,31,30,31];
-    let today = new Date();
-    let currentYear = today.getFullYear();
-    let currentMonth = today.getMonth() + 1;
-    let currentDay = today.getDate();
-    const isValid = true;
-
-    //Validity for empty and non-empty input
-
-    /*if(day_input.value > 31){
-        day_input.style.borderColor = 'hsl(0, 100%, 67%)';
-        dayHead.style.color = 'hsl(0, 100%, 67%)';
-        dayMsg.innerHTML = 'Must be a valid date';
-        isValid = false;
-        return;
-    }
-    else{
-        alert('You can proceed');
-        isValid = true;
-    }
-})*/
